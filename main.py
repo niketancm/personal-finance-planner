5d9e35b2497edaa943367ab291189e6b503f858e
Niketan <niketancm>
2025-06-21 00:00:28 +0530

    initial set-up

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+  return {
+    "message": "Hello, Welcome to Fast-API"
+  }
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+  return{
+    "item_id": item_id,
+    "q": q
+  }
